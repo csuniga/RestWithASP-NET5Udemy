b77e63bb3293554d997d8de12cbb98ee36424600
csuniga <chsuniga@gmail.com>
2022-03-18 07:42:20 -0400

    Working with Content Negotiation

---
+++ 10_RestWithASPNETUdemy_Working with Content Negotiation/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Colider Brasil', 'Miguel', 'Male', 'Suniga'),
+	(2, 'São Paulo Brasil', 'teste', 'Female', 'testelast'),
+	(3, 'Curitiba Brasil', 'junior', 'Male', 'matias'),
+	(4, 'Tangara da serra Brasil', 'Matias', 'Male', 'Suniga');
