b9256bd63f57296faa725ee50fb0d75b949d5f2e
csuniga <chsuniga@gmail.com>
2022-03-25 08:14:39 -0400

    Working with HATEOAS

---
+++ 11_RestWithASPNETUdemy_Working with HATEOAS/RestWithASPNETUdemy/Data/Converter/Implementations/BookConverter.cs
@@ -0,0 +1,53 @@
+﻿using RestWithASPNETUdemy.Data.Converter.Contract;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Data.Converter.Implementations
+{
+    public class BookConverter : IParser<BookVO, Books>, IParser<Books, BookVO>
+    {
+        public BookVO Parse(Books origin)
+        {
+            if (origin == null) return null;
+
+            return new BookVO
+            {
+                Id = origin.Id,
+                Author = origin.Author,
+                LaunchDate = origin.LaunchDate,
+                Price = origin.Price,
+                Title = origin.Title
+            };
+        }
+
+        public List<BookVO> Parse(List<Books> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public Books Parse(BookVO origin)
+        {
+            if (origin == null) return null;
+
+            return new Books
+            {
+                Id = origin.Id,
+                Author = origin.Author,
+                LaunchDate = origin.LaunchDate,
+                Price = origin.Price,
+                Title = origin.Title
+            };
+        }
+
+        public List<Books> Parse(List<BookVO> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
