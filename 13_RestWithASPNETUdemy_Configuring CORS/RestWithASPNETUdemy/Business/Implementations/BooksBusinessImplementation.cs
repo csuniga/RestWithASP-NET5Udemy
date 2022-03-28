b56bc60070b58b8d90def6c07496b5fdd81dfb3c
csuniga <chsuniga@gmail.com>
2022-03-28 07:50:06 -0400

    Configuring CORS

---
+++ 13_RestWithASPNETUdemy_Configuring CORS/RestWithASPNETUdemy/Business/Implementations/BooksBusinessImplementation.cs
@@ -0,0 +1,54 @@
+﻿using RestWithASPNETUdemy.Data.Converter.Implementations;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository;
+using RestWithASPNETUdemy.Repository.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class BooksBusinessImplementation : IBookBusiness
+    {
+        private readonly IRepository<Books> _repository;
+
+        private readonly BookConverter _converter;
+
+        public BooksBusinessImplementation(IRepository<Books> repository)
+        {
+            _repository = repository;
+            _converter = new BookConverter();
+        }
+
+        public List<BookVO> FindAll()
+        {
+            return _converter.Parse(_repository.FindAll());
+        }
+
+        public BookVO FindByID(long id)
+        {
+            return _converter.Parse(_repository.FindByID(id));
+        }
+
+        public BookVO Create(BookVO book)
+        {
+            var bookEntity = _converter.Parse(book);
+            bookEntity = _repository.Create(bookEntity);
+            return _converter.Parse(bookEntity);
+        }
+
+        public BookVO Update(BookVO book)
+        {
+            var bookEntity = _converter.Parse(book);
+            bookEntity = _repository.Update(bookEntity);
+            return _converter.Parse(bookEntity);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
