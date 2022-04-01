583842260549d07a3866ba65eb0899c5cea8d6d1
csuniga <chsuniga@gmail.com>
2022-04-01 11:37:52 -0400

    Working with PATCH Verb

---
+++ 15_RestWithASPNETUdemy_Working with PATCH Verb/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -0,0 +1,26 @@
+﻿using RestWithASPNETUdemy.Hypermedia;
+using RestWithASPNETUdemy.Hypermedia.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class PersonVO : ISupportsHypermedia
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
